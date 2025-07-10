511774df0c90bfa7bf22bfcb0c5de38881cab236
AleksandrTs <ascwork86@gmail.com>
2025-07-10 14:33:24 +0500

    добавил сохранение конвертированных шрифтов в папку + import

--- scripts/convert-fonts.js
+++ scripts/convert-fonts.js
@@ -3,6 +3,9 @@ const path = require('path');
 const ttf2woff = require('ttf2woff');
 
 const FONT_DIR = path.join(__dirname, '../src/assets/fonts');
+const SCSS_FONT_FILE = path.join(__dirname, '../src/styles/_fonts.scss');
+
+let fontFaces = [];
 
 async function walk(dir, callback) {
   for (const file of fs.readdirSync(dir)) {
@@ -35,8 +38,21 @@ async function convertFont(fontPath) {
     fs.writeFileSync(woff2Path, woff2);
     console.log('Created', woff2Path);
   }
+
+  // Генерация @font-face
+  const relFontPath = path.relative(path.join(__dirname, '../src/styles'), fontPath).replace(/\\/g, '/');
+  const fontName = path.basename(fontPath, ext);
+  const fontDir = path.dirname(relFontPath).replace('assets/fonts', 'assets/fonts');
+  const woffRel = relFontPath.replace(ext, '.woff');
+  const woff2Rel = relFontPath.replace(ext, '.woff2');
+  fontFaces.push(`@font-face {\n  font-family: '${fontName}';\n  src: url('../${woff2Rel}') format('woff2'), url('../${woffRel}') format('woff');\n  font-weight: normal;\n  font-style: normal;\n}`);
 }
 
 (async () => {
   await walk(FONT_DIR, convertFont);
+  // Записываем _fonts.scss
+  if (fontFaces.length) {
+    fs.writeFileSync(SCSS_FONT_FILE, fontFaces.join('\n\n'));
+    console.log('Generated', SCSS_FONT_FILE);
+  }
 })(); 
